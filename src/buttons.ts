827531eb5dcadc349de21709b9cda4d004f0350d
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-02-17 21:38:56 +0100

    śro, 17 lut 2021, 21:38:56 CET

--- src/buttons.ts
+++ src/buttons.ts
@@ -1,86 +1,94 @@
+import { BaseComponent } from "./basecomponent.js";
 
-class Button extends HTMLElement {
-    // private name:string;
-    private root;
-    protected sheet:CSSStyleSheet;
 
+class Button extends BaseComponent {
     constructor() {
         super();
-        this.sheet = new CSSStyleSheet();
-        
         this.sheet.insertRule(`button {
+            display: inline-block;
+            transition:.2s ease-out;
             position: relative;
+            overflow: hidden;
+            width: 100%;
+            height: 36px;
+            min-width: 64px;
             margin: 0;
-            color: #000;
-            cursor: pointer;
+            padding: 0 16px;
             border: none;
             border-radius: 2px;
             box-shadow: 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12), 0 2px 4px -1px rgba(0,0,0,.2);
-            display: inline-block;
-            letter-spacing: 0;
-            overflow: hidden;
-            transition:.2s ease-out;
             outline: none;
+            font-size: 14px;
             font-weight: 500;
             text-transform: uppercase;
             text-decoration: none;
             text-align: center;
+            letter-spacing: 0;
             vertical-align: middle;
-            background: rgba(158,158,158,.2);
-        }`,0);
-
-        this.sheet.insertRule(`button {
-            font-size: 14px;
-            height: 36px;
-            width: 100%;
-            min-width: 64px;
-            padding: 0 16px;
             line-height: 36px;
-        }`, 1);
+            color: #000;
+            cursor: pointer;
+            background: rgba(158,158,158,.2);
+        }`, 0);
 
-        this.sheet.insertRule(`button:active {box-shadow:none;}`, 2);
+        this.sheet.insertRule(`button:active {box-shadow:none;}`, 1);
 
-        this.root = this.attachShadow({ mode: 'open'});
     }
+
     public connectedCallback() {
         this.root.adoptedStyleSheets = [ this.sheet ];
         this.root.innerHTML = `<button><slot /></button>`
     }
+
+    static get observedAttributes() {
+        return ['color'];
+    }
+
+    public attributeChangedCallback(name:string, oldValue:string, newValue:string) {
+        console.log(`${name}'s value has been changed from ${oldValue} to ${newValue}`);
+        // switch(name) {
+        //     case "color":
+        //         this.setColor(newValue);
+        //         break;
+
+        // }
+    }
+
+    private setColor(value:string) {
+        console.log(value);
+
+    }
+    
 }
 
 class ButtonSmall extends Button {
     constructor() {
         super();
-        this.sheet.deleteRule(1);
-        this.sheet.insertRule(`button {
-            font-size: 12px;
-            height: 24px;
-            width: 100%;
-            min-width: 48px;
-            padding: 0 8px;
-            line-height: 24px;
-        }`, 1);
+        let rule:CSSStyleRule = this.sheet.rules[0] as CSSStyleRule;
+        rule.style.fontSize = "12px";
+        rule.style.height = "24px";
+        rule.style.width = "100%";
+        rule.style.minWidth = "48px";
+        rule.style.padding = "0 8px";
+        rule.style.lineHeight = "24px";
     }
 }
 
-class ButtonFloating extends ButtonSmall {
+class ButtonFloating extends Button {
     constructor() {
         super();
-        this.sheet.deleteRule(1);
-        this.sheet.insertRule(`button {
-            border-radius: 50%;
-            font-size: 24px;
-            height: 56px;
-            margin: auto;
-            min-width: 56px;
-            width: 56px;
-            padding: 0;
-            box-shadow: 0 1px 1.5px 0 rgba(0,0,0,.12), 0 1px 1px 0 rgba(0,0,0,.24);
-            line-height: normal;
-        }`, 1);
+        let rule:CSSStyleRule = this.sheet.rules[0] as CSSStyleRule;
+        rule.style.borderRadius = "50%";
+        rule.style.fontSize = "24px";
+        rule.style.height = "56px";
+        rule.style.width = "56px";
+        rule.style.minWidth = "56px";
+        rule.style.padding = "0";
+        rule.style.lineHeight = "24px";
+        rule.style.boxShadow = "0 1px 1.5px 0 rgba(0,0,0,.12), 0 1px 1px 0 rgba(0,0,0,.24)";
     }
 }
 
-window.customElements.define('btn-normal', Button);
-window.customElements.define('btn-small', ButtonSmall);
-window.customElements.define('btn-floating', ButtonFloating);
+window.customElements.define('button-normal', Button);
+window.customElements.define('button-small', ButtonSmall);
+window.customElements.define('button-floating', ButtonFloating);
